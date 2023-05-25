b7c127ccd2d6cb129606296d6bd2da86adae3e64
newor0599 <114163256+newor0599@users.noreply.github.com>
2023-05-25 20:59:11 +0800

    fix input keep clearing text AGAIN

--- index.js
+++ index.js
@@ -10,24 +10,28 @@ document.getElementById("query").addEventListener("keyup",function(event){
     if(event.key == "Enter" && search != ""){
         if (search == "y"){
             window.open("https://youtube.com");
+            document.getElementById("query").value = "";
             return
         }
         else if (search == "d"){
             window.open("https://discord.com/channels/@me");
+            document.getElementById("query").value = "";
             return
         }
         else if(search=="g"){
             window.open("https://github.com")
+            document.getElementById("query").value = "";
             return
         }
         else if(search=="r"){
             window.open("https://roblox.com/home")
+            document.getElementById("query").value = "";
             return
         }
         else{
             var url = "https://duckduckgo.com/"+search;
             window.open(url);
+            document.getElementById("query").value = "";
         }
-        document.getElementById("query").value = "";
     }    
 })
