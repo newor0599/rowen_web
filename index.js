89d56c6769bd10f58a74d053298ef2cf233c9963
newor0599 <114163256+newor0599@users.noreply.github.com>
2023-05-25 22:10:35 +0800

    function to check connectivity with the internet
    Added function to check connectivity with the internet
    Changed css body height 100dvh overflow to hidden

--- index.js
+++ index.js
@@ -1,37 +1,48 @@
-document.getElementById("search").onclick = function(){
-    var search = document.getElementById("query").value;
-    if (search!=""){
-        var url = "https://duckduckgo.com/"+search;
-        window.open(url);
+window.onload = checkConnectivity;
+function checkConnectivity(){
+    online = navigator.onLine;
+    if (online){
+
+        document.getElementById("search").onclick = function(){
+            var search = document.getElementById("query").value;
+            if (search!=""){
+                var url = "https://duckduckgo.com/"+search;
+                window.open(url);
+            }
+        }
+        document.getElementById("query").addEventListener("keyup",function(event){
+            var search = document.getElementById("query").value;
+            if(event.key == "Enter" && search != ""){
+                if (search == "y"){
+                    window.open("https://youtube.com");
+                    document.getElementById("query").value = "";
+                    return
+                }
+                else if (search == "d"){
+                    window.open("https://discord.com/channels/@me");
+                    document.getElementById("query").value = "";
+                    return
+                }
+                else if(search=="g"){
+                    window.open("https://github.com")
+                    document.getElementById("query").value = "";
+                    return
+                }
+                else if(search=="r"){
+                    window.open("https://roblox.com/home")
+                    document.getElementById("query").value = "";
+                    return
+                }
+                else{
+                    var url = "https://duckduckgo.com/"+search;
+                    window.open(url);
+                    document.getElementById("query").value = "";
+                }
+            }    
+        })
+    }
+    else{
+        document.querySelector('html').innerHTML = "You dont have internet right now"
     }
 }
-document.getElementById("query").addEventListener("keyup",function(event){
-    var search = document.getElementById("query").value;
-    if(event.key == "Enter" && search != ""){
-        if (search == "y"){
-            window.open("https://youtube.com");
-            document.getElementById("query").value = "";
-            return
-        }
-        else if (search == "d"){
-            window.open("https://discord.com/channels/@me");
-            document.getElementById("query").value = "";
-            return
-        }
-        else if(search=="g"){
-            window.open("https://github.com")
-            document.getElementById("query").value = "";
-            return
-        }
-        else if(search=="r"){
-            window.open("https://roblox.com/home")
-            document.getElementById("query").value = "";
-            return
-        }
-        else{
-            var url = "https://duckduckgo.com/"+search;
-            window.open(url);
-            document.getElementById("query").value = "";
-        }
-    }    
-})
+
