766787ccc650bf71f11d4698596d4914a1af35ca
newor0599 <114163256+newor0599@users.noreply.github.com>
2023-05-25 11:59:54 +0800

    fixed opening two tab at once while using shortcut

--- index.js
+++ index.js
@@ -9,21 +9,24 @@ document.getElementById("query").addEventListener("keyup",function(event){
     var search = document.getElementById("query").value;
     if(event.key == "Enter" && search != ""){
         if (search == "y"){
-            window.open("https://youtube.com");    
+            window.open("https://youtube.com");
+            return
         }
         else if (search == "d"){
             window.open("https://discord.com/channels/@me");
+            return
         }
         else if(search=="g"){
             window.open("https://github.com")
+            return
         }
         else if(search=="r"){
             window.open("https://roblox.com/home")
+            return
         }
         else{
             var url = "https://duckduckgo.com/"+search;
             window.open(url);
-            document.getElementById("query").value = "";
         }
         document.getElementById("query").value = "";
     }    
